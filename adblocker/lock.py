122a05b0e8e93926859e0a821adad018df27677f
Izra Faturrahman <Frizz925@hotmail.com>
2018-11-26 13:45:08 +0700

    Set locked property to a method

--- adblocker/lock.py
+++ adblocker/lock.py
@@ -4,8 +4,11 @@ import os
 class FileLock:
     def __init__(self, lockfile):
         self.lockfile = lockfile
-        self.locked = self.check()
         self.fd = None
+
+    @property
+    def locked(self):
+        return self.check()
 
     def check(self):
         return os.path.exists(self.lockfile)
@@ -14,7 +17,6 @@ class FileLock:
         if self.locked:
             return
         self.fd = os.open(self.lockfile, os.O_CREAT | os.O_EXCL | os.O_RDONLY)
-        self.locked = True
 
     def release(self):
         if not self.locked:
@@ -23,21 +25,14 @@ class FileLock:
             os.close(self.fd)
             self.fd = None
         os.unlink(self.lockfile)
-        self.locked = False
 
     def raise_for_acquire(self):
         if self.locked:
             raise LockStateException('Current state is acquired')
-        if self.check():
-            raise FileExistsError('Lockfile already exists at ' + self.lockfile)
-        if self.fd is not None:
-            raise LockStateException('File descriptor for lockfile already exists! (The lockfile may have been abruptly removed)')
 
     def raise_for_release(self):
         if not self.locked:
             raise LockStateException('Current state is released')
-        if not self.check():
-            raise FileNotFoundError('Lockfile not found at ' + self.lockfile)
 
 
 class LockStateException(Exception):
